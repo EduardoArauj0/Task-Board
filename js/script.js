6a82e8e1000cdde9491ec5775034656b760aa1d5
Eduardo Araujo <eduardoaraujo.contato13@gmail.com>
2025-03-13 17:06:48 -0300

    fix: corrigir eventos de drag and drop e criação de cards

--- js/script.js
+++ js/script.js
@@ -1,58 +1,57 @@
-const columns = document.querySelectorAll('.column__cards');
+const columns = document.querySelectorAll(".column__cards");
 
 let draggedCard;
 
-const dragstart = (event) => {
+const dragStart = (event) => {
     draggedCard = event.target;
-    event.dataTransfer.effectAllowed = 'move';
-}
+    event.dataTransfer.effectAllowed = "move";
+};
 
 const dragOver = (event) => {
     event.preventDefault();
-}
+};
 
-const dragEnter = ({target}) => {
-   if (target.classList.contains('column--cards')) {
-        target.classList.add('column--highlight');
-   }
-}
+const dragEnter = ({ target }) => {
+    if (target.classList.contains("column__cards")) {
+        target.classList.add("column--highlight");
+    }
+};
 
-const dragLeave = ({target}) => {
-    target.classList.remove('column--highlight');
- }
- 
- const drop = ({target}) => {
-    if (target.classList.contains('column--cards')) {
-        target.classList.add('column--highlight');  
+const dragLeave = ({ target }) => {
+    target.classList.remove("column--highlight");
+};
+
+const drop = ({ target }) => {
+    if (target.classList.contains("column__cards")) {
+        target.classList.remove("column--highlight");
         target.append(draggedCard);
-   }
-  
- }
- 
-  const createCard = ({target}) => {
-    if (!target.classList.contains('column--cards')) return;
+    }
+};
 
-    const card = document.createElement('section');
+const createCard = ({ target }) => {
+    if (!target.classList.contains("column__cards")) return;
 
-    card.className = 'card';
-    card.draggable = 'true';
-    card.contentEditable = 'true';
+    const card = document.createElement("section");
 
-    card.addEventListener('foccusout', () => {
-        card.contentEditable = 'false';
+    card.className = "card";
+    card.draggable = "true";
+    card.contentEditable = "true";
+
+    card.addEventListener("focusout", () => {
+        card.contentEditable = "false";
         if (!card.textContent) card.remove();
     });
 
-    card.addEventListener('dragstart', dragstart);
+    card.addEventListener("dragstart", dragStart);
 
     target.append(card);
     card.focus();
-  }
+};
 
 columns.forEach((column) => {
-    column.addEventListener('dragover', dragOver);
-    column.addEventListener('dragenter', dragEnter);
-    column.addEventListener('dragleave', dragLeave);
-    column.addEventListener('drop', drop);
-    column.addEventListener('dblclick', createCard);
-})
+    column.addEventListener("dragover", dragOver);
+    column.addEventListener("dragenter", dragEnter);
+    column.addEventListener("dragleave", dragLeave);
+    column.addEventListener("drop", drop);
+    column.addEventListener("dblclick", createCard);
+});
